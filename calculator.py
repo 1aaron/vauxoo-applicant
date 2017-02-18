c7c21f5baa5cb6d63266b7d2bd279fb8262f4feb
1aaron <aaronnek1@gmail.com>
2017-02-18 16:07:51 -0600

    py2

--- calculator.py
+++ calculator.py
@@ -1,16 +1,21 @@
 """
-Your module documentation here
+A new module for numbers, i saw that you asked for a class named 
+calculator_class so i cahnged the name here and 
+in the all_unittest.py file
 """
 
 
 class CalculatorClass(object):
     """
-    Your class documentation here
+    the class calculator which will work with numbers
     """
 
     def sum(self, num_list):
         """
-        Your method documentation here
+        the sum method that will sum a bunch of numbers 
         """
         # your sum code here
-        return "not implement yet"  # Remove this dummy line
+        suma = 0
+        for number in num_list:
+            suma += number
+        return suma
--- primes.py
+++ primes.py
@@ -3,22 +3,25 @@ Module for practice on python for vauxoo to test me, I want to be an intern :)
 """
 
 
-class prime_class(object):
+class PrimeClass(object):
     """
     this class will help to determine which type of number are you evaluating
     """
 
     def is_prime(self, num_int):
         """
-        here we are going to evaluate if num_int is a prime number (only divisible by itself or 1)
+        here we are going to evaluate if num_int is a prime number 
+        (only divisible by itself or 1)
         """
         # your primes code here
-	divisible = False
-	for i in range(2,num_int-1):
-		if num_int % i == 0:
-			divisible = True
-	if divisible == True:
-		return False
-	else:
-		return True
-		
+        divisible = False
+        if num_int == 0:
+            return True
+        for number in range(2, num_int-1):
+            if num_int % number == 0:
+                divisible = True
+                break
+        if divisible:
+            return False
+        else:
+            return True
